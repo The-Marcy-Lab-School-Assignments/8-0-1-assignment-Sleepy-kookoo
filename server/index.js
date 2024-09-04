c755ffada0f85381f402cdb34a102a259add0de4
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-09-04 13:08:19 -0400

    Completed all set-up, and server application requirements

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express();
+const path = require('path')
+
+const pathToDistFolder = path.join(__dirname, '../vite-project/dist')
+
+const serverStatic = express.static(pathToDistFolder)
+
+const logRoutes = (req, res, next) => {
+  const time = new Date().toLocaleString();
+
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+
+  next();
+}
+
+app.use(logRoutes)
+app.use(serverStatic)
+
+
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`)
+})
+
+
